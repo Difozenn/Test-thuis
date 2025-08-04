1c054578eb32fc61b69ec877373f8f27e1b17349
Difozenn <rob_Vanlooy@hotmail.com>
2025-08-04 22:19:14 +0200

    04/08

--- enterprise-file-monitor/obj/Release/net6.0-windows/win-x64/FileMonitorTray.AssemblyInfo.cs
+++ enterprise-file-monitor/obj/Release/net6.0-windows/win-x64/FileMonitorTray.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec718b7a5e685ee5bee84f6d37b5bf75e802d741")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNC DATALOG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNC DATALOG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
